0212718de01a078ee95a78f7775c7d9c5d6d3daf
Max <qiwi360@gmail.com>
2013-12-18 23:40:47 +0400

    New closure class added: L

---
+++ src/com/danilov/bool4j/test/Test5.java
@@ -0,0 +1,42 @@
+package com.danilov.bool4j.test;
+import ru.matlog.bool4j.closures.ClosureClass;
+import ru.matlog.bool4j.closures.ClosureClasses;
+import ru.matlog.bool4j.expression.Expression;
+import ru.matlog.bool4j.parser.Parser;
+import ru.matlog.bool4j.parser.RecursiveParserImpl;
+
+import com.danilov.bool4j.util.Util;
+
+
+public class Test5 implements ITest {
+
+	@Override
+	public void test() {
+		Parser p = new RecursiveParserImpl();
+		Expression expr = p.parse("(x + y * z + r + (Q <=> e + t + p + f + g + zz) + oi)");
+		Util.Log("Expression: " + expr.toString());
+		ClosureClass cc = ClosureClasses.getClosureClass("T0");
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		cc = ClosureClasses.getClosureClass("T1");
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		cc = ClosureClasses.getClosureClass("S");
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		cc = ClosureClasses.getClosureClass("M");
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		cc = ClosureClasses.getClosureClass("L");
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		
+		expr = p.parse("(x xor (y * z)");
+		Util.Log("Expression: " + expr.toString());
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		expr = p.parse("(x xor (y xor z)");
+		Util.Log("Expression: " + expr.toString());
+		Util.Log("Accessory to the " + cc.getStringRepresentation() + " closure class: " + cc.whetherBelongs(expr));
+		
+		Util.Log("P.S. Real accessory is not tested yet.");
+	}
+
+}
+
+
+
